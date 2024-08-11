f09e3ea8b60315b53c6b38fc8c4a1a97891a98e3
sielcoh <sielcoh@gmail.com>
2024-08-11 18:43:40 +0300

    add a photos from cumputer clients

--- api/index.js
+++ api/index.js
@@ -5,8 +5,12 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js')
 const cookieParser = require('cookie-parser');
+const download = require('image-downloader');
+const multer = require('multer')
+const fs = require('fs')
 require('dotenv').config();
 const app = express();
+
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'asdasd34rfsdzsd333';
@@ -14,6 +18,7 @@ const jwtSecret = 'asdasd34rfsdzsd333';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
     origin: ' http://localhost:5173'
@@ -75,7 +80,33 @@ app.get('/profile', (req, res) => {
 });
 
 app.post('/logout', (req, res) => {
-    res.cookie('token' , '').json(true)
+    res.cookie('token', '').json(true)
+})
+
+app.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    await download.image({
+        url: link,
+        dest: __dirname + '/uploads/' + newName
+    })
+    res.json(newName)
+
+})
+const photosMiddleWare = multer({ dest: 'uploads' })
+app.post('/upload', photosMiddleWare.array('photos', 100), async (req, res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+        const { path, originalname } = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1]
+        const newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+        console.log(newPath.replace('uploads\\', ''));
+        uploadedFiles.push(newPath.replace('uploads\\', ''));
+
+    }
+    res.json(uploadedFiles);
 })
 
 app.listen(4000);
